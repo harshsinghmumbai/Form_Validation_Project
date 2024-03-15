5f70828738ef4d25b571e70c3c542e173362b18b
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-03-15 12:20:05 +0530

    formik part of code is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useFormik } from "formik";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+};
 const App = () => {
+  const { values, errors, touched, handleBlur, handleSubmit, handleChange } =
+    useFormik({
+      initialValues: initialValues,
+      onSubmit: (value) => {
+        console.log(value);
+      },
+    });
+  // console.log(formik);
   return (
     <>
       <div
@@ -21,9 +37,14 @@ const App = () => {
                   Sign In
                 </a>
               </p>
-              <form action="#" method="POST" className="mt-8">
+              <form
+                action="#"
+                method="POST"
+                onSubmit={handleSubmit}
+                className="mt-8"
+              >
                 <div className="space-y-5">
-                  <div>
+                  <div id="Name_field">
                     <label
                       htmlFor="name"
                       className="text-base font-medium text-gray-900"
@@ -33,13 +54,17 @@ const App = () => {
                     <div className="mt-2">
                       <input
                         className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm placeholder:text-gray-400 "
+                        name="name"
                         type="text"
-                        placeholder="Full Name"
                         id="name"
+                        placeholder="Enter Full Name"
+                        value={values.name} // updated value//
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                       ></input>
                     </div>
                   </div>
-                  <div>
+                  <div id="Email_field">
                     <label
                       htmlFor="email"
                       className="text-base font-medium text-gray-900"
@@ -49,13 +74,17 @@ const App = () => {
                     <div className="mt-2">
                       <input
                         className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm placeholder:text-gray-400"
+                        name="email"
                         type="email"
-                        placeholder="Email"
                         id="email"
+                        placeholder="Enter Email"
+                        value={values.email} //updated value//
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                       ></input>
                     </div>
                   </div>
-                  <div>
+                  <div id="password_field">
                     <div className="flex items-center justify-between">
                       <label
                         htmlFor="password"
@@ -67,15 +96,41 @@ const App = () => {
                     <div className="mt-2">
                       <input
                         className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm placeholder:text-gray-400 "
+                        name="password"
                         type="password"
-                        placeholder="Password"
                         id="password"
+                        placeholder="Enter Password"
+                        value={values.password} //updated value//
+                        onChange={handleChange}
+                        onBlur={handleBlur}
                       ></input>
                     </div>
                   </div>
-                  <div>
+                  <div id="confirm_password_filed">
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor="Confirm_Password"
+                        className="text-base font-medium text-gray-900"
+                      >
+                        Confirm Password
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm placeholder:text-gray-400 "
+                        name="confirm_password"
+                        type="password"
+                        id="password"
+                        placeholder="Enter Confirm Password" //updated values//
+                        value={values.confirm_password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      ></input>
+                    </div>
+                  </div>
+                  <div id="submit_button ">
                     <button
-                      type="button"
+                      type="submit"
                       className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                     >
                       Create Account
@@ -87,7 +142,7 @@ const App = () => {
           </div>
           <div id="image" className="p-2 mt-5">
             <img
-              className="mx-auto h-[540px] w-[620px] rounded-md object-cover hidden lg:block"
+              className="mx-auto h-[640px] w-[620px] rounded-md object-cover hidden lg:block"
               src="https://images.unsplash.com/photo-1559526324-4b87b5e36e44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
               alt=""
             />
